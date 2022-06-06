6cc2a0b6ecf1b99eaff75b148ff1f86ee2802d5f
moningfly <79407773+morningfly7012@users.noreply.github.com>
2022-06-06 16:11:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import time
+import json
+import os
+from cProfile import run
+from distutils.log import debug
+from openpyxl import load_workbook
+#用來匯入模組
+
+
+# 讀取 Excel 檔案
+wb = load_workbook('status.xlsx') #載入status.xlsx這個檔案
+sheet = wb['狀態'] #選擇工作列 狀態
+  
+ipp = input('請輸入連線位置：')
+api = requests.get("https://api.mcsrvstat.us/2/"+ipp) #設定API
+apiii = json.loads(api.text) #載入API
+
+while True:
+    m = time.strftime("%M",time.localtime())
+    if m == 00:
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        #更改excel檔案
+        with open ("system.json",mode="r",encoding="utf-8") as filt:
+            data = json.load(filt)     
+            timess = str(data["time"])
+            statuss = str(data["status"])
+            onliness = str(data["online"])
+            timein = sheet["A"+timess]
+            timein.value = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            statusin = sheet["B"+statuss]
+            statusin.value = apiii["online"]
+            onlinein = sheet["C"+onliness]
+            onlinein.value = apiii["players"]["online"]
+            wheretime = data["time"] + 1 #morningfly版權所有
+            wherestatus = data["status"] + 1
+            whereonline = data["online"] + 1
+            wb.save('status.xlsx')
+        
+        #更改json參數 來調等
+        with open ("system.json",mode="w",encoding="utf-8") as filt:
+            datas = {"time":wheretime,"status":wherestatus,"online":whereonline}
+            json.dump(datas,filt)
